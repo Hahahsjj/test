94e2b6705f3b0f4b8687d5cf2e376c0c380b3a5d
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-02 21:00:01 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,150 @@
+START_TXT = """/<b>Hello {}👋,
+
+I am OTT Downloader Bot. I can help you to download content from OTT Platforms.
+
+Made With ❤️ by [_*Shivam*_](httpst.me/Legend_Shivam_7)"""
+
+
+RIP_TXT = """RROR: You Don't Have Any Subscription.
+Check Plans **/plans**"""
+
+
+MYPLAN_TXT = """**You Don't Have Any Subscription.**
+Buy Subscription From [**Shivam**](httpst.me/Legend_Shivam_7)"""
+
+
+PLANS_TXT = """
+🔰OTT Downloader Bot Plans🔰
+
+👇INDIVIDUAL PLANS(All OTTs)👇
+
+➡️ 1 Day      - ₹100
+➡️ 1 Week   - ₹500
+➡️ 1 Month - ₹1700
+
+👇INDIVIDUAL PLANS(Custom OTTs)👇
+
+➡️ 1 Day      - ₹15/OTT
+➡️ 1 Week   - ₹70/OTT
+➡️ 1 Month - ₹220/OTT
+
+👇GROUP PLANS(All OTTs)👇
+
+➡️ 1Day      - ₹300
+➡️ 1Week   - ₹1100
+➡️ 1Month - ₹3500
+
+👇GROUP PLANS(Custom OTTs)👇
+
+➡️ 1Day      - ₹55/OTT
+➡️ 1Week   - ₹210/OTT
+➡️ 1Month - ₹600/OTT
+
+*Max 20 Members are Allowed in Group Plan"""
+
+
+ABOUT_TXT = ""'
+Mʏ Nᴀᴍᴇ: Oᴛᴛ Dᴏᴡɴʟᴏᴀᴅᴇʀ (https://t.me/https://t.me/Ott_Downloader7_bot)
+
+Vᴇʀsɪᴏɴ: 0.1.7-Oᴛᴛ_Dᴏᴡɴʟᴏᴀᴅᴇʀ_Bᴏᴛ_Tɢ
+
+Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3.10.6 (https://www.python.org/)
+
+👨‍🚒Dᴇᴠᴇʟᴏᴘᴇʀ: [Shivam](https://t.me/Legend_Shivam_7)
+
+Sᴘᴏɴsᴏʀᴇᴅ Bʏ: [Telly Fun] (https://t.me/TellyFun2)
+
+
+AVAILABLEOTT_TXT = ""'
+**👇Available OTTs👇
+
+🔹Aha
+🔹AltBalaji
+🔹BookMyShow
+🔹Chaupal
+🔹Hoichoi
+🔹Hotstar
+🔹JioCinema
+🔹MX Player
+🔹Netflix
+🔹OhoGujarati
+🔹Prime Video
+🔹ShemarooMe
+🔹SonyLiv
+🔹SunNXT
+🔹VROTT
+🔹Voot
+🔹VootKids
+🔹Zee5**"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

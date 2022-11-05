06f32c5f5d1ce3c9ebb72a9712721fb55e0e5dac
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-05 15:16:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 
-API_KEY = os.getenv('API_KEY',"")
+API_KEY = os.getenv('API_KEY',"5598963437:AAG9roGF9xQ_9HNiRcpwKdCpGi1k3jCMGKk")
 bot = telebot.Telebot(API_KEY)
 
 @bot.message_handler(command=['start'])

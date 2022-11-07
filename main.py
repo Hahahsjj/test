e271ed46c327423d265b353182673b4743151eb0
mr7hacker <112601204+mr7hacker@users.noreply.github.com>
2022-11-07 14:34:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 import os
-from texts import START_TXT RIP_TXT MYPLAN_TXT PLANS_TXT ABOUT_TXT AVAILABLEOTT_TXT
+from text import START_TXT RIP_TXT MYPLAN_TXT PLANS_TXT ABOUT_TXT AVAILABLEOTT_TXT
 
 
 # bot
@@ -11,6 +11,10 @@ bot_token = os.environ.get("TOKEN", "")
 api_hash = os.environ.get("HASH", "") 
 api_id = os.environ.get("ID", "")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)
+
+@app.on_message(filters.command(["start"]))
+def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    app.send_message(message.chat.id, START_TXT, reply_to_message_id=message.id, disable_web_page_preview=True)
 
 
 print("Bot Starting")
